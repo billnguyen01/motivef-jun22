2af735108b852e06bd8bde702721c7d55b79027c
Jacky <tdhungit@gmail.com>
2022-06-14 14:28:48 +0700

    fixed contact form

--- abk_contact_warning/static/src/js/custom_form_view.js
+++ abk_contact_warning/static/src/js/custom_form_view.js
@@ -4,49 +4,55 @@ odoo.define('custom_form_view.contact_form', function (require) {
     var viewRegistry = require('web.view_registry');
     var FormController = require('web.FormController');
     var FormView = require('web.FormView');
-    var FormRenderer = require('web.FormRenderer');
     var Dialog = require('web.Dialog');
     var core = require('web.core');
 
     var ContactFormController = FormController.extend({
         saveRecord: function (recordID, options) {
-            var self = this;
             var _super = this._super.bind(this);
             var _t = core._t;
 
-            var name = $('input[name="name"]').val();
+            console.log(recordID);
+
+            var name = $('input[name="name"]').val().trim();
+            if (!name) {
+                return this._super.apply(this, arguments);
+            }
+
             var rpc = require('web.rpc');
             var domain = [['name', '=', name]];
 
-            console.log(options);
-            var res = rpc.query({
+            rpc.query({
                 model: 'res.partner',
-                method: 'search',
+                method: 'search_read',
                 args: [domain]
-            }).then(function (partners) {
-                console.log(partners);
+            }).then(function (contacts) {
+                console.log(contacts);
+                if (contacts.length > 0) {
+                    new Dialog(this, {
+                        title: _t("Warning"),
+                        $content: $('<div/>') .append($('<p/>', {text: _t("Duplicate Contact")})),
+                        buttons: [
+                            {
+                                text: _t("Ok"),
+                                classes: 'btn btn-primary',
+                                click: function() {
+                                    this.close();
+                                    return _super(recordID, options);
+                                },
+                                close: true
+                            }, {
+                                text: _t('Cancel'),
+                                close: true
+                            }
+                        ]
+                    }).open();
+        
+                    return Promise.reject("SaveRecord: waiting for check duplicate");
+                }
+
+                return _super(recordID, options);
             });
-
-            new Dialog(this, {
-                title: _t("Warning"),
-                $content: $('<div/>') .append($('<p/>', {text: _t("Duplicate Contact")})),
-                buttons: [
-                    {
-                        text: _t("Ok"),
-                        classes: 'btn btn-primary',
-                        click: function() {
-                            this.close();
-                            return _super(recordID, options);
-                        },
-                        close: true
-                    }, {
-                        text: _t('Cancel'),
-                        close: true
-                    }
-                ]
-            }).open();
-
-            return Promise.reject("SaveRecord: waiting for check duplicate");
         },
     });
 
